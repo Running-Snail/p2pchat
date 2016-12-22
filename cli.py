6698889878e3d253469447e72670d0baa9458c75
ZhihaoJun <zhao11fs@gmail.com>
2016-12-23 00:27:14 +0800

    thread management

--- cli.py
+++ cli.py
@@ -2,6 +2,9 @@ import server
 import client
 import threading
 import time
+import sys
+import os
+import signal
 
 sobj = None
 cobj = None
@@ -9,6 +12,7 @@ peer_id = None
 pause = False
 
 def start_server(port):
+    global sobj
     sobj = server.P2PChatServer(port)
     print 'start reciving'
     while 1:
@@ -18,9 +22,11 @@ def start_server(port):
 def server_command(port):
     port = int(port)
     t = threading.Thread(target=start_server, args=(port, ))
+    t.daemon = True
     t.start()
 
 def start_client(remote, port):
+    global cobj
     cobj = client.P2PChatClient(remote, port)
     while 1:
         cobj.recv()
@@ -30,15 +36,18 @@ def client_command(remote_ip, remote_port, port):
     remote_port = int(remote_port)
     port = int(port)
     t = threading.Thread(target=start_client, args=((remote_ip, remote_port), port))
+    t.daemon = True
     t.start()
 
 def register_command():
+    global cobj
     if cobj is not None:
         cobj.register()
     else:
         print 'no client'
 
 def connect_command(device_id):
+    global cobj
     def connected(cli, device_id):
         peer_id = device_id
         pause = False
@@ -50,17 +59,26 @@ def connect_command(device_id):
         print 'no client'
 
 def say_command(text):
+    global cobj
     if cobj is not None:
         cobj.send(peer_id, text)
     else:
         print 'no client'
 
 def list_command():
-    print sobj
+    global sobj
     if sobj is not None:
         sobj.list()
     else:
         print 'no server'
+
+def exit_command():
+    if sobj is not None:
+        sobj.close()
+    if cobj is not None:
+        cobj.close()
+    print 'bye'
+    sys.exit(0)
 
 handlers = {
     'server': server_command,
@@ -68,14 +86,23 @@ handlers = {
     'register': register_command,
     'connect': connect_command,
     'say': say_command,
-    'list': list_command
+    'list': list_command,
+    'exit': exit_command
 }
 
+def signal_handler(signal, frame):
+    sys.exit(0)
+
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
     while 1:
         while pause:
             time.sleep(0.2)
-        command = raw_input('>> ')
+        try:
+            command = raw_input('>> ')
+        except EOFError as e:
+            print 'bye'
+            break
         parts = command.split(' ')
         action = parts[0]
         if action in handlers:
